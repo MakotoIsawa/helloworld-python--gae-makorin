f70ffe0d286e201e8cc5e3d5c3d9b016f5e5a34c
Makoto Isawa <makoto.isawa@gmail.com>
2014-06-02 16:33:23 +0900

    added escape function

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+import cgi
 import webapp2
 
 
@@ -5,9 +6,18 @@ form = """
 <form method="post">
 	What is your birthday?  
   <br>
-  <label>Month <input type="text" name="month"></label> 
-  <label>Day <input type="text" name="day"></label>
-  <label>Year  <input type="text" name="year"></label>
+  <label>
+      Month 
+      <input type="text" name="month" value="%(month)s">
+  </label> 
+  <label>
+      Day 
+      <input type="text" name="day" value="%(day)s">
+  </label>
+  <label>
+      Year  
+      <input type="text" name="year" value="%(year)s">
+      </label>
   <div style="color: red">%(error)s</div>
     <br>
     <br>
@@ -51,22 +61,35 @@ def valid_year(year):
             return year
 
 
+def escape_html(s):
+    return cgi.escape(s, quote = True)
 
 
 class MainPage(webapp2.RequestHandler):
-    def write_form(self, error=""):
-      self.response.out.write(form % {"error": error})
+    def write_form(self, error="", month="", day="", year=""):
+      self.response.out.write(form % {"error": error,
+                                      "month": escape_html(month),
+                                      "day": escape_html(day),
+                                      "year": escape_html(year)})
 
     def get(self):
         self.write_form()
 
     def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_day = valid_day(self.request.get('day'))
-        user_year = valid_year(self.request.get('year'))
+        #user_month = valid_month(self.request.get('month'))
+        #user_day = valid_day(self.request.get('day'))
+        #user_year = valid_year(self.request.get('year'))
+        user_month = self.request.get("month")
+        user_day = self.request.get("day")
+        user_year = self.request.get("year")
 
-        if not (user_month and user_day and user_year):
-            self.write_form()
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+
+        if not (month and day and year):
+            self.write_form("That does't look valid to me, friend.", user_month, user_day, user_year)
         else:
             self.response.out.write("Thanks! That's totally valid day!!")
 
